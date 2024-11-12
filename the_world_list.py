7c7022dbc627c8657ad50464b370d6fb813c2f88
TH <tommyhh04.kodehode@gmail.com>
2024-11-12 10:48:58 +0100

    Made list, and sorted it alphabetical

---
+++ the_world_list.py
@@ -0,0 +1,8 @@
+#Ønskeliste over plasser jeg ønsker å besøke i ingen spesifikk rekkefølge
+
+ønskeliste = ["hawaii", "new zealand", "japan", "sør-korea", "thailand", "bora bora"]
+
+#Ønskelisten blir sortert i alfabetisk rekkefølge
+ønskeliste.sort()
+
+print(ønskeliste)
